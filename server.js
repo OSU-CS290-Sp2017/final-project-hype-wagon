36623de2f71da55d338d74eb76108f2a8363d706
Kyle Nichols <nichokyl@onid.oregonstate.edu>
2017-06-12 20:14:24 -0700

    Set up for Node.js handling for .html and .css

---
+++ server.js
@@ -0,0 +1,46 @@
+var http = require("http");
+var fs = require("fs");
+
+console.log("Starting server...");
+
+var server = http.createServer(requestHandler);
+
+function handleHTML(response) {
+	var data = fs.readFileSync("./index.html");
+	
+	response.write(data);
+}
+
+function handleCSS(response) {
+	var data = fs.readFileSync("./style.css");
+	
+	response.write(data);
+}
+
+function requestHandler(request, response) {
+//	console.log("URL: " + request.url);
+	response.statusCode = 200;
+	if (request.url == "/index.html") {
+		response.setHeader("Content-Type", "text/html");
+//		console.log("HTML");
+		handleHTML(response);
+	} else if (request.url == "/style.css") {
+		response.setHeader("Content-Type", "text/css");
+//		console.log("CSS");
+		handleCSS(response);
+	} else if (request.url == "/") {
+		response.setHeader("Content-Type", "text/html");
+//		console.log("/");
+		handleHTML(response);
+	}
+	
+	response.end();
+}
+
+if (process.env.PORT) {
+//	console.log("Using defined");
+	server.listen(PORT);
+} else {
+//	console.log("Using 3000");
+	server.listen(3000);
+}
