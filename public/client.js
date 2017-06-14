ce7451f341e31d47f9906a3191e7fa32fda464b5
James May <jimwmay2@gmail.com>
2017-06-14 00:07:22 -0700

    search entry
    added search function and listener

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-var add_trailer_button = document.querySelector("#add-trailer-button");
+var add_trailer_button = document.querySelector("add-trailer-button");
 add_trailer_button.addEventListener("click", function() {
     var modal_backdrop = document.querySelector("#modal-backdrop");
     var modal = document.querySelector("#add-trailer-modal");
@@ -10,13 +10,13 @@ add_trailer_button.addEventListener("click", function() {
 function close_modal() {
 	var trailer_title = document.querySelector('#trailer-title');
 	trailer_title.value = "";
-	
+
 	var trailer_url = document.querySelector('#trailer-url');
 	trailer_url.value = "";
-	
+
 	var modal_backdrop = document.querySelector('#modal-backdrop');
 	modal_backdrop.classList.add('hidden');
-	
+
 	var add_trailer_modal = document.querySelector('#add-trailer-modal');
 	add_trailer_modal.classList.add('hidden');
 }
@@ -26,35 +26,48 @@ modal_close_button.addEventListener('click', close_modal);
 var modal_cancel_button = document.querySelector('.modal-cancel-button');
 modal_cancel_button.addEventListener('click', close_modal);
 
+var trailerary = document.getElementsByClassName("trailer")
+var searchbutton = document.getElementById("navbar-search-button")
+function searchs(){
+  for(var i = 0; i < trailerary.length; i++){
+    var title = document.getElementsByClassName("trailerInfoItem trailer-title")[i].textContent;
+    var tags = document.getElementsByClassName("trailerInfoItem trailer-tags")[i].textContent;
+    if(title.search(searchbar.value) === -1 && tags.search(searchbar.value) === -1){
+      trailerary[i].style.display = 'none';
+    }
+  }
+}
+searchbutton.addEventListener('click', searchs);
+
 var modal_accept_button = document.querySelector('.modal-accept-button');
 modal_accept_button.addEventListener('click', function (event) {
-	var trailer_title = document.querySelector('#trailer-title');
-	var trailer_url = document.querySelector('#trailer-url');
-	
-	if (!trailer_title.value || !trailer_url.value) {
-		alert("Please fill out all fields.");
-		return;
-	}
-	
-	var post_url = "/trailers/addTrailer";
+  var trailer_title = document.querySelector('#trailer-title');
+  var trailer_url = document.querySelector('#trailer-url');
 
-    var request = new XMLHttpRequest();
-    request.open("POST", post_url);
-    request.setRequestHeader("Content-Type", "application/json");
+  if (!trailer_title.value || !trailer_url.value) {
+	   alert("Please fill out all fields.");
+	    return;
+  }
 
-    request.addEventListener("load", function(event) {
-        var error;
-        if (event.target.status !== 200) {
-            error = event.target.response;
-        }
-        callback(error);
+  var post_url = "/trailers/addTrailer";
+
+  var request = new XMLHttpRequest();
+  request.open("POST", post_url);
+  request.setRequestHeader("Content-Type", "application/json");
+
+  request.addEventListener("load", function(event) {
+    var error;
+    if (event.target.status !== 200) {
+      error = event.target.response;
+    }
+    callback(error);
     });
 
-    var post_body = {
-        title: trailer_title,
-        url: trailer_url
-    };
-    request.send(JSON.stringify(post_body));
-	
+  var post_body = {
+    title: trailer_title,
+    url: trailer_url
+  };
+  request.send(JSON.stringify(post_body));
+
 	close_modal();
 });
